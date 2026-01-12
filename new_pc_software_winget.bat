e8ac2dfa6a16dd69902539791aeca42564f86e2b
mbayat82 <116715813+mbayat82@users.noreply.github.com>
2026-01-12 20:55:52 +1100

    Add files via upload

---
+++ new_pc_software_winget.bat
@@ -0,0 +1,71 @@
+@echo off
+setlocal enabledelayedexpansion
+echo Checking for Winget...
+
+:: Verify winget is available
+where winget >nul 2>nul
+if %ERRORLEVEL% NEQ 0 (
+    echo [ERROR] Winget is not installed or not in your PATH.
+    echo Please go to the Microsoft Store and update "App Installer".
+    pause
+    exit /b
+)
+
+echo Starting Winget Installation Script...
+echo ---------------------------------------
+
+:: Use the ^ symbol at the end of each line to continue the list
+:: This makes it very easy to add/remove apps later.
+set apps_id=^
+ Warp.Warp ^
+ Google.Chrome ^
+ Fork.Fork ^
+ SublimeHQ.SublimeText.4 ^
+ Microsoft.VisualStudioCode ^
+ AgileBits.1Password ^
+ Valve.Steam ^
+ Plex.PlexMediaServer ^
+ REALiX.HWiNFO ^
+ Guru3D.Afterburner ^
+ LocalSend.LocalSend ^
+ Google.GoogleDrive ^
+ Microsoft.PowerToys ^
+ Amazon.AWSCLI ^
+ Microsoft.OneDrive ^
+ Notion.Notion ^
+ qBittorrent.qBittorrent ^
+ RevoUninstaller.RevoUninstaller ^
+ Wagnardsoft.DisplayDriverUninstaller ^
+ voidtools.Everything ^
+ ShareX.ShareX ^
+ 9NKSQGP7F2NH ^
+ Logitech.OptionsPlus ^
+ Logitech.LogiBolt ^
+ VideoLAN.VLC ^
+ ALCPU.CoreTemp ^
+ Insta360.Link.Controller ^
+ dotPDN.PaintDotNet ^
+ CodeSector.TeraCopy ^
+ Postman.Postman ^
+ 7zip.7zip
+
+set apps_name=^
+ Whatsapp
+
+:: Loop through the app id's
+for %%a in (%apps_id%) do (
+    echo.
+    echo Installing: %%a
+    winget install -e --id %%a --accept-package-agreements --accept-source-agreements
+)
+
+:: Loop through the app names
+for %%a in (%apps_name%) do (
+    echo.
+    echo Installing: %%a
+    winget install %%a --accept-package-agreements --accept-source-agreements
+)
+
+echo ---------------------------------------
+echo All tasks complete!
+pause
